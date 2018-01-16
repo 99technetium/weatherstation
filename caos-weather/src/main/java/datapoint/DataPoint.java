e7c8e3d6dade73fbfc3b87f26304826f3095639c
Max Hackinger <mahaxi@gmail.com>
2018-01-16 01:43:42 +0100

    added getters to datapoint class

--- caos-weather/src/main/java/datapoint/DataPoint.java
+++ caos-weather/src/main/java/datapoint/DataPoint.java
@@ -2,17 +2,39 @@ package datapoint;
 
 /**
  * The DataPoint class represents a single datapoint that was received from the sensor station.
- * DataPoints are returned when queering the SQL database through the WeatherObject.
+ * DataPoints are returned when queering the SQL database through the WeatherObject and are sent
+ * to the Base station form the sensor Station.
  *
  * A data point contains a value, unit, date and time of the measurement.
  */
 
 public class DataPoint {
-    private String sensorID;
-    private String deviceID;
-    private double value;
-    private int date;
-    private int time;
+    private final String deviceID;
+    private final String sensorID;
+    private final double value;
+    private final int date;
+    private final int time;
+
+    public String getDeviceID() {
+        return deviceID;
+    }
+
+    public String getSensorID() {
+        return sensorID;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
     public DataPoint(String deviceID, String sensorID, double value, int date, int time){
         this.deviceID = deviceID;
         this.sensorID = sensorID;
