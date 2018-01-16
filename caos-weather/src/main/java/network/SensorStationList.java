80bbf80943825d2aaf31e1cc26da5f98f3940266
Max Hackinger <mahaxi@gmail.com>
2018-01-16 01:42:56 +0100

    List to for all connected sensorstations

---
+++ caos-weather/src/main/java/network/SensorStationList.java
@@ -0,0 +1,25 @@
+package network;
+
+
+/**
+ * A list of all base stations that are connected to the base station.
+ * This is used mostly so that when the SensorStation connects to the BaseStation, the BaseStation can see this and send
+ * updates to the SensorStation.
+ */
+public class SensorStationList {
+    private static SensorStationList instance = new SensorStationList();
+
+    public static SensorStationList getInstance() {
+        return instance;
+    }
+
+    private SensorStationList(){}
+
+    //TODO: implement function
+    public void addStation(String key, SensorStation sensorStation){
+    }
+
+    //TODO: implement function
+    public void removeStation(SensorStation sensorStation){
+    }
+}
