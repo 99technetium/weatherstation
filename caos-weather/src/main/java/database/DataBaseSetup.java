7db58f203336bafbfbb093ffde785913b0c0b97a
Max Hackinger <mahaxi@gmail.com>
2018-01-22 10:37:19 +0100

    changed databasesetup to use initalconfig methode in databasemanager

--- caos-weather/src/main/java/database/DataBaseSetup.java
+++ caos-weather/src/main/java/database/DataBaseSetup.java
@@ -1,25 +1,25 @@
 package database;
 
 public class DataBaseSetup {
+    //TODO: use a trully random password
     static String PASS = "bad_password";
 
-    DataBaseConfig config = new DataBaseConfig();
+        DataBaseConfig config = new DataBaseConfig();
 
     public DataBaseSetup(){
         config.createPropFile(PASS);
 
         DataBaseManager dataBaseManager = new DataBaseManager();
 
-        DataBaseManager dbManager = new DataBaseManager();
+
 
         String sql = "CREATE TABLE WEATHER_DATA "
                 + "(DEVICE_ID BIGINT NOT NULL GENERATED ALWAYS AS IDENTITY, "
                 + "SENSOR_ID BIGINT, "
-                + "UNIT INTEGER, "
                 + "VALUE BIGINT, "
                 + "DATE INTEGER, "
                 + "TIME INTEGER) ";
 
-        dbManager.configDatabase(sql);
+        dataBaseManager.initalConfig(sql);
     }
 }
