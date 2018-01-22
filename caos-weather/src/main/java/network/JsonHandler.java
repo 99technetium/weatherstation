7913417553180d2a2d75a9043c430463b57adc13
Max Hackinger <mahaxi@gmail.com>
2018-01-22 10:24:24 +0100

    created jsonhandler to convert json strings in to datapoints

---
+++ caos-weather/src/main/java/network/JsonHandler.java
@@ -0,0 +1,23 @@
+package network;
+
+import com.google.gson.Gson;
+import datapoint.DataPoint;
+
+public class JsonHandler {
+    String inputString;
+    public JsonHandler(String inputString) {
+        this.inputString = inputString;
+    }
+
+    public String getJson(){
+        return "";
+    }
+
+    public DataPoint getDatapoint(){
+        String jsonString = inputString;
+        DataPoint dataPoint;// = new DataPoint("","",1,1,1);
+        Gson gson = new Gson();
+        dataPoint = gson.fromJson(jsonString, DataPoint.class);
+        return dataPoint;
+    }
+}
