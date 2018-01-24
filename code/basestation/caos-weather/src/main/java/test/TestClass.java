29f6ba9f0c2b92509b01a4bda3236223772c66f8
Max Hackinger <mahaxi@gmail.com>
2018-01-24 17:06:11 +0100

    updated testclass to try adding and querying datapoints

--- code/basestation/caos-weather/src/main/java/test/TestClass.java
+++ code/basestation/caos-weather/src/main/java/test/TestClass.java
@@ -1,22 +1,41 @@
 package test;
 
 import database.DataBaseConfig;
+import database.DataBaseManager;
 import database.DataBaseSetup;
 import database.WeatherObject;
+import datapoint.DataPoint;
 
-import java.io.IOException;
+import java.util.List;
 
 public class TestClass {
 
     /**
-     * This is a simple test main class to be able to test the Networking,
-     * and first setup process
+     * This is a simple main class to be able to test the Networking,
+     * first setup process and database queries
      * @param args
      */
     public static void main(String[] args){
-        DataBaseSetup setup = new DataBaseSetup();
+        DataBaseConfig config = new DataBaseConfig();
 
-        WeatherObject weatherObject = new WeatherObject(5, 0);
+        if(!config.isDbConfiged()){
+            new DataBaseSetup();
+        }
+
+        DataBaseManager manager = new DataBaseManager();
+        manager.addDataPoint(new DataPoint(124132, 2, 3, 4, 20180124, 1511));
+        manager.addDataPoint(new DataPoint(436513, 2, 3, 4, 20180120, 1511));
+        manager.addDataPoint(new DataPoint(234366, 2, 3, 123, 20180123, 1511));
+
+
+        WeatherObject weatherObject = new WeatherObject(20180122, 20180125);
+
+        List<DataPoint> dataPoints = weatherObject.getByType(3);
+
+        for (DataPoint d : dataPoints) {
+            System.out.println(d.getDeviceID());
+        }
+
        // System.out.println("used weather api");
     }
 }
