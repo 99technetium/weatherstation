8029ff25abfbf4687b2a5162038f8f3a07766d14
Max Hackinger <mahaxi@gmail.com>
2018-01-24 17:05:37 +0100

    implemented getByType methode

--- code/basestation/caos-weather/src/main/java/database/WeatherObject.java
+++ code/basestation/caos-weather/src/main/java/database/WeatherObject.java
@@ -1,54 +1,84 @@
 package database;
 
+import datapoint.DataPoint;
+
+import java.util.List;
+
 /**
- * WeatherObject creates an object that can be used to query the collected data.
+ * WeatherObject creates an object that can be used toDate query the collected data.
  */
 public class WeatherObject {
-    private int deviceID;
-    private int unit;
+    private int fromDate;
+    private int toDate;
 
     /**
-     * Constructor for a WeatherObject object if one of the unites is set to 0,
-     * only functions that do not need the specific unite for their call can be used.
+     * Constructor is called with parameters fromDate toDate which define the time period in which to search for DataPoints
      *
-     * @param deviceID id of device for which data is to be queried
-     * @param unit unit of data that is to be queried
+     * @param fromDate defines from what past data the query should start looking
+     * @param toDate defines to what date the query should look
      */
-    public WeatherObject(int deviceID, int unit){
-        this.deviceID = deviceID;
-        this.unit = unit;
+    public WeatherObject(int fromDate, int toDate){
+        this.fromDate = fromDate;
+        this.toDate = toDate;
     }
 
     /**
      * If the Constructor is called without any Parameters,
-     * the object can only be used to query the devices that have connected to the base station
+     * WeatherObject returns all available DataPoints that match the query
      */
     public WeatherObject(){
-        deviceID = 0;
-        unit = 0;
+        fromDate = 0;
+        toDate = 0;
     }
 
-    //TODO: implement function
-    public void getAllDevices(){}
 
-    //TODO: implement function
-    public void getAllUnits(){}
+    /**
+     * method returns all available types in database
+     */
+    //TODO: implement method
+    public void availableTypes(){}
 
-    //TODO: implement function
-    public void getDataBetween(int startDate, int startTime, int endDate, int endTime){}
+    /**
+     * function searches for and returns all values of a given type
+     *
+     * @param type for which to search the database
+     * @return List of DataPoints
+     */
+    //TODO: implement method
+    public List<DataPoint> getByType(int type){
+        DataBaseManager manager = new DataBaseManager();
+        String sql;
 
+        if(fromDate != 0 && toDate != 0) {
+            sql = "SELECT DEVICE_ID, SENSOR_ID, TYPE, VALUE, DATE, TIME FROM WEATHER_DATA "
+                    + "WHERE TYPE = " + type + " "
+                    + "AND (DATE >= " + fromDate + " "
+                    + "AND DATE <= " + toDate + " )";
+        }
+        else {
+            sql = "SELECT DEVICE_ID, SENSOR_ID, TYPE, VALUE, DATE, TIME FROM WEATHER_DATA "
+                    + "WHERE TYPE = " + type + " ";
+        }
 
+        List<DataPoint> dataPoints = manager.getDataPoint(sql);
 
-    //TODO: implement function
+        return dataPoints;
+    }
+
+    //TODO: remove unused code and functions
+    //public void getAllDevices(){}
+
+    //public void getAllUnits(){}
+
+    //public void getDataBetween(int startDate, int startTime, int endDate, int endTime){}
+
     //public DataPoint getSpecificDataPoint(int date, int time){
     //    DataPoint data = new DataPoint("dkkdkdk134","asdfasdf", 1, 12022017,132430);
     //    return data;
     //}
 
-    //TODO: implement function
     //public void getAllDataPointsFromDevice(){}
 
-    //TODO: implement function
     //public void getAllDataPointsPerUnit(){}
 
     ///**
@@ -56,16 +86,14 @@ public class WeatherObject {
      //* @param unit
      //* @return
      //*/
-    //TODO: implement function
+
     //public int getTotalPerUnit(String unit){
     //    int total = 0;
     //    return total;
     //}
 
-    //TODO:
     //public int getDateOfEarliestUnit(){return 5;}
 
-    //TODO:
     //public void getAvgForTimeInterval(int intervel, int unitType){
         //return
     //}
