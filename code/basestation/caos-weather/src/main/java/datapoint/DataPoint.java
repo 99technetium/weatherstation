594c83ae5e54c96b4f2de017f3075e774137ba8b
Max Hackinger <mahaxi@gmail.com>
2018-01-24 16:53:29 +0100

    updated datapoint so that all values are integers

--- code/basestation/caos-weather/src/main/java/datapoint/DataPoint.java
+++ code/basestation/caos-weather/src/main/java/datapoint/DataPoint.java
@@ -10,25 +10,30 @@ package datapoint;
  */
 
 public class DataPoint {
-    private final String deviceID;
-    private final String sensorID;
+    private final int deviceID;
+    private final int sensorID;
+    private final int type;
     private final double value;
     private final int date;
     private final int time;
 
-    public String getDeviceID() {
+    public int getDeviceID() {
         return deviceID;
     }
 
-    public String getSensorID() {
+    public int getSensorID() {
         return sensorID;
+    }
+
+    public int getType() {
+        return type;
     }
 
     public double getValue() {
         return value;
     }
 
-    public int getDate() {
+    public long getDate() {
         return date;
     }
 
@@ -36,9 +41,20 @@ public class DataPoint {
         return time;
     }
 
-    public DataPoint(String deviceID, String sensorID, double value, int date, int time){
+    /**
+     * A DataPoint is immutable and all values are initialized in the constructor.
+     *
+     * @param deviceID unique id for sensor station device
+     * @param sensorID unique id for sensor
+     * @param type of data that was measured, example temperature
+     * @param value of measurement preformed by sensor station
+     * @param date with YYYYMMDD, example 20180124, this makes it very easy to compare dates
+     * @param time with HHMM, example 1506, this makes it very easy to compare times
+     */
+    public DataPoint(int deviceID, int sensorID,int type, double value, int date, int time){
         this.deviceID = deviceID;
         this.sensorID = sensorID;
+        this.type = type;
         this.value = value;
         this.date = date;
         this.time = time;
